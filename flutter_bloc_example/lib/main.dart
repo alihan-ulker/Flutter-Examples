96a17200b5d927d9cb00d84f6858149055a5719a
ali-ulker <ali.ulker.217@gmail.com>
2022-09-09 15:41:28 +0300

    images added

--- flutter_bloc_example/lib/main.dart
+++ flutter_bloc_example/lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Bloc Example',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
