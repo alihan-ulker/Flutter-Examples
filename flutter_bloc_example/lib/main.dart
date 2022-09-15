eed44a8ea5c12b48d9dc68cfeb9f315fcee21b61
ali-ulker <ali.ulker.217@gmail.com>
2022-09-15 15:52:45 +0300

    multi provider added

--- flutter_bloc_example/lib/main.dart
+++ flutter_bloc_example/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc_example/core/models/project_context_model.dart';
+import 'package:flutter_bloc_example/core/models/user_context_model.dart';
 import 'package:flutter_bloc_example/feature/login/view/login_view.dart';
+import 'package:flutter_bloc_example/feature/onboard/on_board_view.dart';
+import 'package:provider/provider.dart';
 //import 'package:flutter_bloc_example/feature/onboard/on_board_view.dart';
 
 void main() {
@@ -12,20 +16,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Bloc Example',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-          appBarTheme: const AppBarTheme(
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            systemOverlayStyle: SystemUiOverlayStyle.dark,
-          ),
-          scaffoldBackgroundColor: Colors.grey[300],
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-              backgroundColor: Color.fromRGBO(11, 23, 84, 1))),
-      //home: const OnBoardView(),
-      home: const LoginView(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => UserContext("a")),
+        ChangeNotifierProvider(create: (context) => ProductContext()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Bloc Example',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+            appBarTheme: const AppBarTheme(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              systemOverlayStyle: SystemUiOverlayStyle.dark,
+            ),
+            scaffoldBackgroundColor: Colors.grey[300],
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+                backgroundColor: Color.fromRGBO(11, 23, 84, 1))),
+        home: const OnBoardView(),
+        //home: const LoginView(),
+      ),
     );
   }
 }
