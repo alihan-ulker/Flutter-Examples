43fa824f4d2e7a8d590dd927373c805aec78c70a
ali-ulker <ali.ulker.217@gmail.com>
2022-09-22 15:35:10 +0300

    Update states_widget.dart

--- flutter_bloc_example_v2/lib/src/users/view/sub_view/states_widget.dart
+++ flutter_bloc_example_v2/lib/src/users/view/sub_view/states_widget.dart
@@ -1,8 +1,22 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc_example_v2/src/users/view_model/cubit/users_cubit_state.dart';
 
 extension UsersInitialWidget on UsersInitial {
   Widget buildWidget() {
     return const Text("Okeyy");
+  }
+}
+
+extension UsersListItemWidget on UsersListItemState {
+  Widget buildWidget() {
+    return ListView.builder(itemBuilder: (context, index) {
+      return Card(
+        child: ListTile(
+          title: Text(items[index].firstName ?? ""),
+        ),
+      );
+    });
   }
 }
